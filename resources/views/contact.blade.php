8a08a45aae50e3ad3d64bea2018e52e75bba30a6
HemlataJadhav04 <164496711+HemlataJadhav04@users.noreply.github.com>
2024-07-03 16:50:26 +0530

    contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -19,59 +19,144 @@
     </div>
 </div>
 
-<section class="sec-contact">
+<section>
 	<div class="container">
-		<h2 class="blue pb-60 text-center f700">Contact Us</h2>  
-		<div class="row pb-60 ">
-			<div class="col-md-5">
-				<h3 class="blue f600">MITR Hospital, Kharghar</h3>
-				<a href="tel:+916262840940" class="mb-3"><i class="fas fa-mobile-alt"></i> Call Us at :  +91 6262 840 940 or +91 6262 690 790</a>
-				<p><i class="fas fa-map-marker-alt"></i> Plot # 13, Off Uran Road, Parsik Hill Rd, Sector 23, CBD Belapur, Navi Mumbai, Maharashtra 400614</p>
-				<p><i class="fas fa-clock"></i> Monday-Saturday: 12pm to 2pm and 06:30pm to 09:00pm</p>
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-12 ft-contact-cta-items">
+				<div class="d-flex">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-location-arrow" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>MITR Hospital, Kharghar</h3>
+						<p>Plot # 13, Off Uran Road, Parsik Hill Rd, Sector 23, CBD Belapur, Navi Mumbai, Maharashtra 400614</p>
+					</div>
+				</div>
+				<div class="d-flex pt-4">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-phone" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>Phone</h3>
+						<p><a href="tel:+916262840940">+91 6262 840 940 or +91 6262 690 790</a></p>
+					</div>
+				</div>
+				<div class="d-flex pt-4">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-clock" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>Timings</h3>
+						<p>Monday-Saturday: 12pm to 2pm and 06:30pm to 09:00pm</p>
+					</div>
+				</div>
 			</div>
-			<div class="col-md-7">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d60345.66672634202!2d72.9881116!3d19.0371562!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c23ecbea307f%3A0xc51ce74eb4a7c67!2sDr%20Bharat%20Kumar!5e0!3m2!1sen!2sin!4v1668509544480!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-			</div>
-		</div>
-
-		<div class="row pb-60 ">
-			<div class="col-md-5">
-				<h3 class="blue f600">MGM Hospital, Vashi</h3>
-				<a href="tel:02250666777"><i class="fas fa-mobile-alt"></i> Call us at: (022) 5066 6777</a>
-				<p><i class="fas fa-map-marker-alt"></i> Plot no - 35, Sector 3, Vashi, Navi Mumbai, Maharashtra 400703</p>
-				<p><i class="fas fa-clock"></i> Monday - Saturday: 04:00pm to 06:00pm</p>
-			</div>
-			<div class="col-md-7">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60332.65450430959!2d72.92798033125004!3d19.072928699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c0cff7a1da49%3A0x5208e691d43368c5!2sDr%20Bharat%20Kumar!5e0!3m2!1sen!2sin!4v1668509615565!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-			</div>
-		</div>
-
-		<div class="row pb-60">
-			<div class="col-md-5">
-				<h3 class="blue f600">Aayush Hospital, Kalyan</h3>
-				<a href="tel:+918767040404"><i class="fas fa-mobile-alt"></i> Call us at: +91 8767 040 404</a>
-				<p><i class="fas fa-map-marker-alt"></i> Building No. E2, 2nd & 3rd Floor, Radha Nagar Shopping Complex, Radha Nagar, Khadakpada, Kalyan, Mumbai 421301</p>
-				<p><i class="fas fa-clock"></i> Only Wednesdays: 11am - 1pm</p>
-			</div>
-			<div class="col-md-7">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3857018.0737212324!2d68.91935699999996!3d19.25542030000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be79686e15929b1%3A0xcc95df7b89d248e1!2sAayush%20Multispeciality%20Hospital%20-%20Emergency%20%26%20Critical%20Hospital%20in%20Kalyan!5e0!3m2!1sen!2sin!4v1668509832767!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			<div class="col-lg-6 col-md-6 col-12">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d120691.24710370477!2d73.05856!3d19.037275!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c23ecbea307f%3A0xc51ce74eb4a7c67!2sDr%20Bharat%20Kumar!5e0!3m2!1sen!2sin!4v1720002621485!5m2!1sen!2sin" width="100%" height="410" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 			</div>
 		</div>
-		
-			<div class="row pb-60">
-			<div class="col-md-5">
-				<h3 class="blue f600">Apollo Hospitals, Belapur</h3>
-				<a href="tel:02262806280"><i class="fas fa-mobile-alt"></i> Call Us at :  02262806280</a>
-				<p><i class="fas fa-map-marker-alt"></i> Plot # 13, Off Uran Road, Parsik Hill Rd, Sector 23, CBD Belapur, Navi Mumbai, Maharashtra 400614</p>
-				<p><i class="fas fa-clock"></i> By Appointments</p>
+		<div class="row pt-4">
+			<div class="col-lg-6 col-md-6 col-12 ft-contact-cta-items">
+				<div class="d-flex">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-location-arrow" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>MGM Hospital, Vashi</h3>
+						<p>Plot no - 35, Sector 3, Vashi, Navi Mumbai, Maharashtra 400703</p>
+					</div>
+				</div>
+				<div class="d-flex pt-4">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-phone" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>Phone</h3>
+						<p><a href="tel:02250666777">(022) 5066 6777</a></p>
+					</div>
+				</div>
+				<div class="d-flex pt-4">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-clock" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>Timings</h3>
+						<p>Monday - Saturday: 04:00pm to 06:00pm</p>
+					</div>
+				</div>
 			</div>
-			
-	
-			<div class="col-md-7">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m19!1m8!1m3!1d60336.39370831472!2d72.9742052!3d19.0626557!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x3be7c3903a3c9297%3A0xb858a25d34856005!2sApollo%20Hospitals%20Mumbai%2C%20Plot%20%23%2013%2C%20Off%20Uran%20Road%2C%20Parsik%20Hill%20Rd%2C%20Sector%2023%2C%20CBD%20Belapur%2C%20Navi%20Mumbai%2C%20Maharashtra%20400614!3m2!1d19.0205055!2d73.02894049999999!5e0!3m2!1sen!2sin!4v1693316127607!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			<div class="col-lg-6 col-md-6 col-12">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d120665.30879017235!2d72.993898!3d19.072929!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c0cff7a1da49%3A0x5208e691d43368c5!2sDr%20Bharat%20Kumar%20-%20Best%20Orthopedic%20Surgeon%20%2F%20Doctor%20in%20Navi%20Mumbai%2C%20Hip%2C%20Knee%20Joint%20Replacement%20Surgeon%20Vashi%2C%20Navi%20Mumbai!5e0!3m2!1sen!2sin!4v1720004364714!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+		</div>
+		<div class="row pt-4">
+			<div class="col-lg-6 col-md-6 col-12 ft-contact-cta-items">
+				<div class="d-flex">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-location-arrow" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>Aayush Hospital, Kalyan</h3>
+						<p>Building No. E2, 2nd & 3rd Floor, Radha Nagar Shopping Complex, Radha Nagar, Khadakpada, Kalyan, Mumbai 421301</p>
+					</div>
+				</div>
+				<div class="d-flex pt-4">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-phone" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>Phone</h3>
+						<p><a href="tel:+918767040404">+91-8767040404</a></p>
+					</div>
+				</div>
+				<div class="d-flex pt-4">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-clock" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>Timings</h3>
+						<p>Only Wednesdays: 11am - 1pm</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-12">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7714036.16155178!2d73.138107!3d19.25542!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be79686e15929b1%3A0xcc95df7b89d248e1!2sAayush%20Hospital!5e0!3m2!1sen!2sin!4v1720004799893!5m2!1sen!2sin" width="100%" height="390" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+		</div>
+		<div class="row pt-4">
+			<div class="col-lg-6 col-md-6 col-12 ft-contact-cta-items">
+				<div class="d-flex">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-location-arrow" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>Apollo Hospitals, Belapur</h3>
+						<p>Plot # 13, Off Uran Road, Parsik Hill Rd, Sector 23, CBD Belapur, Navi Mumbai, Maharashtra 400614</p>
+					</div>
+				</div>
+				<div class="d-flex pt-4">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-phone" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>Phone</h3>
+						<p><a href="tel:02262806280">02262806280</a></p>
+					</div>
+				</div>
+				<div class="d-flex pt-4">
+					<div class="ft-contact-cta-icon d-flex align-items-center justify-content-center">
+						<i class="fa fa-clock" aria-hidden="true"></i>
+					</div>
+					<div class="ft-contact-cta-text headline pera-content">
+						<h3>Timings</h3>
+						<p>By Appointments</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-12">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.997709839685!2d73.02670407497594!3d19.01982258217319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3b00fa82c01%3A0x4efbb5073f2f8300!2sApollo%20Hospitals%2C%2013%2C%20Road%2C%20off%20Uran%20Road%2C%20opp.%20Nerul%20Wonders%20Park%2C%20Parsik%20Hill%2C%20Sector%2023%2C%20CBD%20Belapur%2C%20Mumbai%2C%20Navi%20Mumbai%2C%20Maharashtra%20400614!5e0!3m2!1sen!2sin!4v1720005186407!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 			</div>
 		</div>
 	</div>
 </section>
-
 @stop
