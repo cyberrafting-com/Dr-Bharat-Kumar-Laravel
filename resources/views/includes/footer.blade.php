d3506bd5239973bbef13ca036a837962112722dd
HemlataJadhav04 <164496711+HemlataJadhav04@users.noreply.github.com>
2024-07-04 20:11:44 +0530

    call button

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -41,14 +41,24 @@
     </div>
   </div>
 
-  <div id="mybutton" class="navbar-fixed-bottom d-lg-none d-xl-none d-md-none d-sm-none">
+  <!-- <div id="mybutton" class="navbar-fixed-bottom d-lg-none d-xl-none d-md-none d-sm-none">
     <div class="d-flex">
         <div style="width: 100%;">
             <a href="tel:+916262840940"><button class="call-us btn" type="button"><img src="{{ asset('/resources/assets/images/phone-icon.png')}}" alt="Call Us">Call Us</button></a>
         </div>
-        <!-- <div style="width: 100%;">
+        <div style="width: 100%;">
             <a href="https://api.whatsapp.com/send?phone=+916262840940&amp;text=book%20an%20appointment"><button class="whatsapp btn" type="button"><img src="{{ asset('/resources/assets/images/whatsapp.png')}}" alt="WhatsApp">WhatsApp</button></a>
-        </div> -->
+        </div>
+    </div> -->
+
+    <div class="footer-stick d-lg-none d-xl-none d-md-none d-sm-none">
+      <div class="row">
+        <div class="col-12 call-btn">
+          <a href="tel:+916262840940" class="text-white">
+            <img src="{{ asset('/resources/assets/images/phone-icon.png')}}" alt="">
+            &nbsp;&nbsp;<span>Call Now</span></a>
+        </div>
+      </div>
     </div>
 
   <script src="{{ asset('/resources/assets/js/jquery-3.3.1.slim.min.js')}}"></script>
