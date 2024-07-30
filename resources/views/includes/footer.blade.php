6dd9f8b36934fc4f5c14a169201512243755620b
shraddhamane98 <167803008+shraddhamane98@users.noreply.github.com>
2024-07-30 13:08:09 +0530

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,93 +1,69 @@
-   <section class="footer">  
-    <div class="container-fluid">
-      <div class="row foot-links">        
-        <div class="col-md-3">
-          <a class="navbar-brand" href="{{route('home')}}"><img src="{{ asset('/resources/assets/images/dr-bharat-logo.png')}}" alt=""></a>
-          <p class="pt-3">Dr. Bharatkumar Raju, Consultant Orthopedic & Joint Replacement Surgeon and Robotic Knee Replacement Surgeon in Navi Mumbai, offers expert care for optimal recovery.</p>
-        </div>    
-        <div class="col-md-3">
-          <div class="footer-directions">
-            <p class="footer-title"><b>Contact Us</b></p>  
-            <div class="footer-info-contact">
-              <i class="fa fa-phone" aria-hidden="true"></i>
-              <h3>Phone</h3>
-              <span><a href="tel:+91 6262 840 940">+91 6262 840 940</a></span><br>
-              <span><a href="tel:+91 6262 690 790">+91 6262 690 790</a></span>
-            </div>  
-            <p class="footer-title"><b>Stay Connected</b></p>
-            <a href="https://www.facebook.com/BharatKumarOrthopaedic" target="_blank"><img src="{{ asset('/resources/assets/images/facebook-icon.svg')}}" class="footer-icon"></a>
-            <a href="https://www.google.com/maps/place/Dr+Bharat+Kumar/@19.0371562,73.0581494,17z/data=!3m1!4b1!4m5!3m4!1s0x3be7c23ecbea307f:0xc51ce74eb4a7c67!8m2!3d19.0371562!4d73.0581494" target="_blank"><img src="{{ asset('/resources/assets/images/google-icon.svg')}}" class="footer-icon"></a>                 
-            <!-- <a href="https://www.youtube.com/@dr.bharatkumar4602" target="_blank"><img src="{{ asset('/resources/assets/images/youtube-icon13.png')}}" class="footer-icon"></a>             -->
-          </div>
-        </div>
-        <div class="col-md-3">
-          <p class="footer-title"><b>Our Location</b></p>  
-          <div class="footer-info-contact">
-            <i class="fa fa-location-arrow" aria-hidden="true"></i>
-            <h3>MITR Hospital, Kharghar</h3>
-            <span><a href="https://maps.app.goo.gl/L2JCTT2fNFy7xYEj9" target="_blank">Plot # 13, Off Uran Road, Parsik Hill Rd, Sector 23, CBD Belapur, Navi Mumbai, Maharashtra 400614</a></span>
-          </div> 
-        </div>
-        <div class="col-md-3">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d120691.24710370477!2d73.05856!3d19.037275!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c23ecbea307f%3A0xc51ce74eb4a7c67!2sDr%20Bharat%20Kumar!5e0!3m2!1sen!2sin!4v1719820828054!5m2!1sen!2sin" width="100%" height="320" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-      </div>
-    </div>
-  </section>
 
-  <div class="copyright-content">
-    <div class="container text-center">
-      <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All Rights Reserved, <span class="d-inline-block">By Dr Bharat Kumar</span></p>
-    </div>
-  </div>
+<section class="footer">
+<div class="container">
+<div class="row foot-links">
+<div class="col-md-3">
+<a class="navbar-brand" href="https://drbharatkumar.com"><img src="https://drbharatkumar.com/resources/assets/images/dr-bharat-logo.png" alt></a>
+<p class="pt-3">Dr. Bharatkumar Raju, Consultant Orthopedic & Joint Replacement Surgeon and Robotic Knee Replacement Surgeon in Navi Mumbai, offers expert care for optimal recovery.</p>
+</div>
+<div class="col-md-3">
+<p class="footer-title"><b>Our Location</b></p>
+<div class="footer-info-contact">
+<i class="fa fa-location-arrow" aria-hidden="true"></i>
+<h5 style="font-size:17px ">MITR Hospital, Kharghar</h5>
+<span><a style="font-size:15px " href="https://maps.app.goo.gl/L2JCTT2fNFy7xYEj9" target="_blank">Plot # 13, Off Uran Road, Parsik Hill Rd, Sector 23, CBD Belapur, Navi Mumbai, Maharashtra 400614</a></span>
+</div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d120691.24710370477!2d73.05856!3d19.037275!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c23ecbea307f%3A0xc51ce74eb4a7c67!2sDr%20Bharat%20Kumar!5e0!3m2!1sen!2sin!4v1719820828054!5m2!1sen!2sin" width="100%" height="auto" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+<div class="col-md-3">
+<div class="footer-directions">
+<p class="footer-title"><b>Contact Us</b></p>
+<div class="footer-info-contact">
+<i class="fa fa-phone" aria-hidden="true"></i>
+<h3>Phone</h3>
+<span><a href="tel:+91 6262 840 940">+91 6262 840 940</a></span><br>
+<span><a href="tel:+91 6262 690 790">+91 6262 690 790</a></span>
+</div>
+<p class="footer-title"><b>Stay Connected</b></p>
+<a href="https://www.facebook.com/BharatKumarOrthopaedic" target="_blank"><img src="https://drbharatkumar.com/resources/assets/images/facebook-icon.svg" class="footer-icon"></a>
+<a href="https://www.google.com/maps/place/Dr+Bharat+Kumar/@19.0371562,73.0581494,17z/data=!3m1!4b1!4m5!3m4!1s0x3be7c23ecbea307f:0xc51ce74eb4a7c67!8m2!3d19.0371562!4d73.0581494" target="_blank"><img src="https://drbharatkumar.com/resources/assets/images/google-icon.svg" class="footer-icon"></a>
 
-  <!-- <div id="mybutton" class="navbar-fixed-bottom d-lg-none d-xl-none d-md-none d-sm-none">
-    <div class="d-flex">
-        <div style="width: 100%;">
-            <a href="tel:+916262840940"><button class="call-us btn" type="button"><img src="{{ asset('/resources/assets/images/phone-icon.png')}}" alt="Call Us">Call Us</button></a>
-        </div>
-        <div style="width: 100%;">
-            <a href="https://api.whatsapp.com/send?phone=+916262840940&amp;text=book%20an%20appointment"><button class="whatsapp btn" type="button"><img src="{{ asset('/resources/assets/images/whatsapp.png')}}" alt="WhatsApp">WhatsApp</button></a>
-        </div>
-    </div> -->
-
-    <div class="footer-stick d-lg-none d-xl-none d-md-none d-sm-none">
-      <!-- <div class="row">
-        <div class="col-12 call-btn">
-          <a href="tel:+916262840940" class="text-white">
-            <img src="{{ asset('/resources/assets/images/phone-icon.png')}}" alt="">
-            &nbsp;&nbsp;<span>Call Now</span></a>
-        </div>
-      </div> -->
-
-        <!-- mobile/whtsapp button start-->
- <div class="navbar-fixed-bottom d-lg-none d-xl-none d-md-none d-sm-none row "
-     style="text-align: center;position: fixed;right: 0;bottom: 0;left: 0;padding: 4px;background-color:lightgray;text-align: center;z-index:29">
-
-     <div class="d-lg-none d-xl-none d-md-none d-sm-none col-12" style=" background-color: #018f99;padding:8px;  ">
-         <a href="tel:+916262840940" style="color:white; font-size: 18px; text-decoration: none;">
-             <img src="{{ asset('/resources/assets/images/phone-icon.png')}}"
-                 style="width: 32px; text-decoration: none;" alt="Call"> Call Us</a>
-     </div>
-     
- </div>
- <!-- mobile/whtsapp button end-->
-    </div>
-
-  
+</div>
+</div>
+<div class="col-md-3">
+<p class="footer-title"><b>Follow Us</b></p>
+<div class="channelvideo">
+<a href="https://youtube.com/@drbharatkumar?si=B506aryFzXXm2eKl">
+<img src="https://drbharatkumar.com/resources/assets/images/you-tube-ch.jpg" class="img-fluid w-100 channelvideo1" alt="Dr. Bharat Kumar You-Tube">
+</a>
+</div>
 
 
+</div>
+</div>
+</div>
+</section>
+<div class="copyright-content">
+<div class="container text-center">
+<p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All Rights Reserved, <span class="d-inline-block">By Dr. Bharat Kumar</span></p>
+</div>
+</div>
+
+<div class="footer-stick d-lg-none d-xl-none d-md-none d-sm-none">
 
 
+<div class="navbar-fixed-bottom d-lg-none d-xl-none d-md-none d-sm-none row " style="text-align: center;position: fixed;right: 0;bottom: 0;left: 0;padding: 4px;background-color:lightgray;text-align: center;z-index:29">
+<div class="d-lg-none d-xl-none d-md-none d-sm-none col-12" style=" background-color: #018f99;padding:8px;  ">
+<a href="tel:+916262840940" style="color:white; font-size: 18px; text-decoration: none;">
+<img src="https://drbharatkumar.com/resources/assets/images/phone-icon.png" style="width: 32px; text-decoration: none;" alt="Call"> Call Us</a>
+</div>
+</div>
 
-
-
-
-
-  <script src="{{ asset('/resources/assets/js/jquery-3.3.1.slim.min.js')}}"></script>
-  <script src="{{ asset('/resources/assets/js/bootstrap.min.js')}}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
-  <script src="{{ asset('/resources/assets/js/owl.carousel.min.js')}}"></script>
-  <script src="{{ asset('/resources/assets/js/script.js')}}"></script>
+</div>
+<script src="https://drbharatkumar.com/resources/assets/js/jquery-3.3.1.slim.min.js"></script>
+<script src="https://drbharatkumar.com/resources/assets/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+<script src="https://drbharatkumar.com/resources/assets/js/owl.carousel.min.js"></script>
+<script src="https://drbharatkumar.com/resources/assets/js/script.js"></script> </div>
